5a46abaa16f8e7646f09217914f31a5fec60c1a0
Simo Tumelius <simo@datamie.fi>
2021-11-21 10:24:22 +0200

    Change 'dbt-cloud job run' args are now generated using DbtCloudJobRunArgs.click_options decorator

--- dbt_cloud/job.py
+++ dbt_cloud/job.py
@@ -1,7 +1,10 @@
 import requests
+import os
 from enum import IntEnum
-from typing import Optional
+from typing import Optional, List
+from pydantic import Field
 from dbt_cloud.account import DbtCloudAccount
+from dbt_cloud.args import ArgsBaseModel
 
 
 class DbtCloudJobRunStatus(IntEnum):
@@ -42,6 +45,52 @@ class DbtCloudJob(DbtCloudAccount):
             account_id=self.account_id,
             api_token=self.api_token,
         )
+
+
+class DbtCloudJobRunArgs(ArgsBaseModel):
+    api_token: str = Field(
+        default_factory=lambda: os.environ["DBT_CLOUD_API_TOKEN"],
+        description="API authentication key",
+    )
+    account_id: int = Field(
+        default_factory=lambda: os.environ["DBT_CLOUD_ACCOUNT_ID"],
+        description="Numeric ID of the Account that the job belongs to",
+    )
+    job_id: int = Field(
+        default_factory=lambda: os.environ["DBT_CLOUD_JOB_ID"],
+        description="Numeric ID of the job to run",
+    )
+    cause: str = Field(
+        default="Triggered via API",
+        description="A text description of the reason for running this job",
+    )
+    git_sha: Optional[str] = Field(
+        description="The git sha to check out before running this job"
+    )
+    git_branch: Optional[str] = Field(
+        description="The git branch to check out before running this job"
+    )
+    schema_override: Optional[str] = Field(
+        description="Override the destination schema in the configured target for this job"
+    )
+    dbt_version_override: Optional[str] = Field(
+        description="Override the version of dbt used to run this job"
+    )
+    threads_override: Optional[int] = Field(
+        description="Override the number of threads used to run this job"
+    )
+    target_name_override: Optional[str] = Field(
+        description="Override the target.name context variable used when running this job"
+    )
+    generate_docs_override: Optional[bool] = Field(
+        description="Override whether or not this job generates docs (true=yes, false=no)"
+    )
+    timeout_seconds_override: Optional[int] = Field(
+        description="Override the timeout in seconds for this job"
+    )
+    steps_override: Optional[List[str]] = Field(
+        description="Override the list of steps for this job"
+    )
 
 
 class DbtCloudJobRun(DbtCloudAccount):
