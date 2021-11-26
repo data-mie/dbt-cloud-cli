0b5a9534c82cea2e58c8a96be51eb9f343b034af
Simo Tumelius <simo@datamie.fi>
2021-11-26 18:11:25 +0200

    Change 'dbt-cloud job run' now returns JSON response and has a --wait boolean flag

--- dbt_cloud/cli.py
+++ dbt_cloud/cli.py
@@ -1,4 +1,4 @@
-import os
+import json
 import time
 import click
 from dbt_cloud.job import DbtCloudJob, DbtCloudJobRunStatus, DbtCloudJobRunArgs
@@ -16,19 +16,25 @@ def job():
 
 @job.command()
 @DbtCloudJobRunArgs.click_options
-def run(**kwargs):
+@click.option(
+    f"--wait/--no-wait",
+    default=False,
+    help="Wait for the process to finish before returning from the API call."
+)
+def run(wait, **kwargs):
     args = DbtCloudJobRunArgs(**kwargs)
     job = DbtCloudJob(**args.dict())
-    click.echo(f"Trigger dbt Cloud job {job.job_id}")
-    job_run = job.run(args=args)
-    click.echo(f"   - Job run ID: {job_run.job_run_id}")
-    click.echo(f"   - Job run payload: {args.get_payload()}")
-    while True:
-        time.sleep(5)
-        status = job_run.get_status()
-        click.echo(f"   - Job run status: {status.name}")
-        if status == DbtCloudJobRunStatus.SUCCESS:
-            break
-        elif status in (DbtCloudJobRunStatus.ERROR, DbtCloudJobRunStatus.CANCELLED):
-            raise Exception("Failure!")
-    click.echo(f"dbt Cloud job run {job_run.job_run_id} finished!")
+    response, job_run = job.run(args=args)
+    if wait:
+        click.echo(f"Trigger dbt Cloud job {job.job_id}")
+        click.echo(f"   - Job run ID: {job_run.job_run_id}")
+        click.echo(f"   - Job run payload: {args.get_payload()}")
+        while True:
+            time.sleep(5)
+            response, status = job_run.get_status()
+            click.echo(f"   - Job run status: {status.name}")
+            if status == DbtCloudJobRunStatus.SUCCESS:
+                break
+            elif status in (DbtCloudJobRunStatus.ERROR, DbtCloudJobRunStatus.CANCELLED):
+                raise Exception("Failure!")
+    click.echo(json.dumps(response.json(), indent=2))
