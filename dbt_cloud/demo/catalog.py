554fd3c1970aa2967548e1b8d14919a1cda62c35
Simo Tumelius <simo@datamie.fi>
2022-02-08 13:56:14 +0200

    Add interactivity using inquirer

--- dbt_cloud/demo/catalog.py
+++ dbt_cloud/demo/catalog.py
@@ -4,12 +4,40 @@ from pydantic import Field
 from dbt_cloud.command.command import DbtCloudBaseModel
 
 
+class Column(DbtCloudBaseModel):
+    """Represents a column in the Catalog."""
+
+    type: str
+    index: int
+    name: str
+    comment: Optional[str]
+
+    def __str__(self):
+        return f"{self.name} (type: {self.type}, index: {self.index}, comment: {self.comment})"
+
+
+class Node(DbtCloudBaseModel):
+    """Represents a node in the Catalog."""
+
+    unique_id: str
+    metadata: Dict[str, Optional[str]]
+    columns: Dict[str, Column]
+    stats: Dict[str, Dict]
+
+    @property
+    def name(self):
+        return self.metadata["name"]
+
+    def __str__(self):
+        return f"{self.name} (type: {self.metadata['type']}, schema: {self.metadata['schema']}, database: {self.metadata['database']})"
+
+
 class Catalog(DbtCloudBaseModel):
     """Represents a dbt catalog.json artifact."""
 
     metadata: Dict
-    nodes: Dict
-    nodes: Dict
+    nodes: Dict[str, Node]
+    sources: Dict
     errors: Optional[Dict]
 
 
@@ -20,5 +48,33 @@ def catalog():
 
 @catalog.command(help="Explores a dbt catalog.json artifact.")
 def explore(**kwargs):
+    import inquirer
+
     catalog = Catalog.parse_file("catalog.json")
-    print(catalog)
+    nodes = {node.name: node for node in catalog.nodes.values()}
+
+    while True:
+        attribute_options = [
+            inquirer.List(
+                "attribute",
+                message="Select attribute to explore",
+                choices=["metadata", "sources", "nodes"],
+            )
+        ]
+        attribute = inquirer.prompt(attribute_options)["attribute"]
+
+        if attribute == "nodes":
+            while True:
+                node_options = [
+                    inquirer.List("node", message="Select node", choices=nodes.values())
+                ]
+                node_selected = inquirer.prompt(node_options)["node"]
+                click.echo(f"{node_selected.name} columns:")
+                for column in node_selected.columns.values():
+                    click.echo(f"- {column}")
+                if not click.confirm("Explore another node?"):
+                    break
+        else:
+            click.echo(getattr(catalog, attribute))
+        if not click.confirm("Explore another attribute?"):
+            break
