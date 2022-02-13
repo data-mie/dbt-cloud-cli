5a78b177c6b3555cf4b8a9b2adb1300d65535e39
Simo Tumelius <simo@datamie.fi>
2022-02-13 13:50:53 +0200

    Add CatalogExploreCommand abstraction

--- dbt_cloud/demo/catalog.py
+++ dbt_cloud/demo/catalog.py
@@ -1,9 +1,12 @@
 import click
+from enum import Enum
+from pathlib import Path
 from typing import Optional, Dict, Any, List
+from pydantic import BaseModel, Field
 from dbt_cloud.command.command import DbtCloudBaseModel
 
 
-class Stats(DbtCloudBaseModel):
+class Stats(BaseModel):
     """Represent node stats in the Catalog."""
 
     id: str
@@ -16,7 +19,7 @@ class Stats(DbtCloudBaseModel):
         return f"{self.label}: {self.value}"
 
 
-class Column(DbtCloudBaseModel):
+class Column(BaseModel):
     """Represents a column in the Catalog."""
 
     type: str
@@ -28,7 +31,7 @@ class Column(DbtCloudBaseModel):
         return f"{self.name} (type: {self.type}, index: {self.index}, comment: {self.comment})"
 
 
-class Node(DbtCloudBaseModel):
+class Node(BaseModel):
     """Represents a node in the Catalog."""
 
     unique_id: str
@@ -52,21 +55,17 @@ class Node(DbtCloudBaseModel):
     def type(self):
         return self.metadata["type"]
 
-    @property
-    def owner(self):
-        return self.metadata.get("owner")
-
-    def __str__(self):
-        return f"{self.name} (type: {self.type}, schema: {self.schema}, database: {self.database})"
-
     def __gt__(self, other):
         return self.name > other.name
 
     def __lt__(self, other):
         return self.name < other.name
 
+    def __str__(self):
+        return f"{self.name} (type: {self.type}, schema: {self.schema}, database: {self.database})"
 
-class Catalog(DbtCloudBaseModel):
+
+class Catalog(BaseModel):
     """Represents a dbt catalog.json artifact."""
 
     metadata: Dict
@@ -75,69 +74,84 @@ class Catalog(DbtCloudBaseModel):
     errors: Optional[Dict]
 
 
-def explore_nodes(nodes: List[Node], node_type: str = "node"):
-    import inquirer
-
-    while True:
-        databases = sorted(set(map(lambda x: x.database, nodes)))
-        database_options = [
-            inquirer.List("database", message="Select database", choices=databases)
-        ]
-        database = inquirer.prompt(database_options)["database"]
-        nodes_filtered = list(filter(lambda x: x.database == database, nodes))
-
-        schemas = sorted(set(map(lambda x: x.schema, nodes_filtered)))
-        schema_options = [
-            inquirer.List("schema", message="Select schema", choices=schemas)
-        ]
-        schema = inquirer.prompt(schema_options)["schema"]
-        nodes_filtered = list(filter(lambda x: x.schema == schema, nodes_filtered))
-
-        node_options = [
-            inquirer.List("node", message="Select node", choices=sorted(nodes_filtered))
-        ]
-        node = inquirer.prompt(node_options)["node"]
-        click.echo(f"{node.name} columns:")
-        for column in node.columns.values():
-            click.echo(f"- {column}")
-        click.echo("")
-        for stats in node.stats.values():
-            if stats.id == "has_stats":
-                continue
-            click.echo(stats)
-        if not click.confirm(f"Explore another {node_type}?"):
-            break
+class NodeType(Enum):
+    NODE = "node"
+    SOURCE = "source"
 
 
-@click.command(help="An inteactive application for exploring catalog artifacts.")
-@click.option(
-    "-f",
-    "--file",
-    default="catalog.json",
-    type=str,
-    help="Catalog file path.",
-)
-def data_catalog(file):
-    import inquirer
-    from art import tprint
+class CatalogExploreCommand(DbtCloudBaseModel):
+    """An inteactive application for exploring catalog artifacts."""
 
-    catalog = Catalog.parse_file(file)
-    nodes = list(catalog.nodes.values())
-    sources = list(catalog.sources.values())
-    tprint("Data Catalog", font="rand-large")
-    while True:
-        attribute_options = [
-            inquirer.List(
-                "attribute",
-                message="Select attribute to explore",
-                choices=["sources", "nodes"],
-            )
-        ]
-        attribute = inquirer.prompt(attribute_options)["attribute"]
+    file: Path = Field(default="catalog.json", description="Catalog file path.")
 
-        if attribute == "nodes":
-            explore_nodes(nodes)
-        elif attribute == "sources":
-            explore_nodes(sources, node_type="source")
-        if not click.confirm("Explore another attribute?"):
-            break
+    def get_catalog(self) -> Catalog:
+        return Catalog.parse_file(self.file)
+
+    def execute(self):
+        import inquirer
+        from art import tprint
+
+        catalog = self.get_catalog()
+        nodes = list(catalog.nodes.values())
+        sources = list(catalog.sources.values())
+        tprint("Data Catalog", font="rand-large")
+        while True:
+            attribute_options = [
+                inquirer.List(
+                    "attribute",
+                    message="Select attribute to explore",
+                    choices=["sources", "nodes"],
+                )
+            ]
+            attribute = inquirer.prompt(attribute_options)["attribute"]
+
+            if attribute == "nodes":
+                self.explore_nodes(nodes)
+            elif attribute == "sources":
+                self.explore_nodes(sources, node_type=NodeType.SOURCE)
+            if not click.confirm("Explore another attribute?"):
+                break
+
+    @classmethod
+    def explore_nodes(cls, nodes: List[Node], node_type: NodeType = NodeType.NODE):
+        """Interactive exploration of nodes or sources to explore and display their metadata"""
+        import inquirer
+
+        while True:
+            databases = sorted(set(map(lambda x: x.database, nodes)))
+            database_options = [
+                inquirer.List("database", message="Select database", choices=databases)
+            ]
+            database = inquirer.prompt(database_options)["database"]
+            nodes_filtered = list(filter(lambda x: x.database == database, nodes))
+
+            schemas = sorted(set(map(lambda x: x.schema, nodes_filtered)))
+            schema_options = [
+                inquirer.List("schema", message="Select schema", choices=schemas)
+            ]
+            schema = inquirer.prompt(schema_options)["schema"]
+            nodes_filtered = list(filter(lambda x: x.schema == schema, nodes_filtered))
+
+            node_options = [
+                inquirer.List(
+                    "node", message="Select node", choices=sorted(nodes_filtered)
+                )
+            ]
+            node = inquirer.prompt(node_options)["node"]
+            click.echo(f"{node.name} columns:")
+            for column in node.columns.values():
+                click.echo(f"- {column}")
+            click.echo("")
+            for stats in node.stats.values():
+                if stats.id == "has_stats":
+                    continue
+                click.echo(stats)
+            if not click.confirm(f"Explore another {node_type.value}?"):
+                break
+
+
+@click.command(help=CatalogExploreCommand.get_description())
+@CatalogExploreCommand.click_options
+def data_catalog(**kwargs):
+    command = CatalogExploreCommand.from_click_options(**kwargs)
+    command.execute()
