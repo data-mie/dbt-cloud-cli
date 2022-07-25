e4b1915516cffeeaedb4e27688533915e1119591
Simo Tumelius <simo.tumelius@gmail.com>
2022-07-25 11:31:10 +0300

    Add Docker image (#64)
    * Add Dockerfile for dbt-cloud-cli container image
    
    * Add more descriptive error messages for missing args
    
    * Add .dockerignore file
    
    Co-authored-by: Simo Tumelius <simo@datamie.fi>

--- dbt_cloud/field.py
+++ dbt_cloud/field.py
@@ -12,29 +12,36 @@ class PythonLiteralOption(click.Option):
             raise click.BadParameter(value)
 
 
+def get_env(name: str, default=None, allow_none: bool = False):
+    value = os.getenv(name, default=default)
+    if not allow_none and value is None:
+        raise ValueError(f"{name} is not defined")
+    return value
+
+
 DBT_CLOUD_HOST_FIELD = Field(
-    default_factory=lambda: os.getenv("DBT_CLOUD_HOST", default="cloud.getdbt.com"),
+    default_factory=lambda: get_env("DBT_CLOUD_HOST", default="cloud.getdbt.com"),
     description="dbt Cloud Host (defaults to 'cloud.getdbt.com' unless DBT_CLOUD_HOST environment variable is set)",
 )
 
 API_TOKEN_FIELD = Field(
-    default_factory=lambda: os.environ["DBT_CLOUD_API_TOKEN"],
+    default_factory=lambda: get_env("DBT_CLOUD_API_TOKEN"),
     description="API authentication key (default: DBT_CLOUD_API_TOKEN environment variable)",
 )
 ACCOUNT_ID_FIELD = Field(
-    default_factory=lambda: os.environ["DBT_CLOUD_ACCOUNT_ID"],
+    default_factory=lambda: get_env("DBT_CLOUD_ACCOUNT_ID"),
     description="Numeric ID of the dbt Cloud account (default: DBT_CLOUD_ACCOUNT_ID environment variable)",
 )
 PROJECT_ID_FIELD = Field(
-    default_factory=lambda: os.environ["DBT_CLOUD_PROJECT_ID"],
+    default_factory=lambda: get_env("DBT_CLOUD_PROJECT_ID"),
     description="Numeric ID of a dbt Cloud project (default: DBT_CLOUD_PROJECT_ID environment variable)",
 )
 ENVIRONMENT_ID_FIELD = Field(
-    default_factory=lambda: os.environ["DBT_CLOUD_ENVIRONMENT_ID"],
+    default_factory=lambda: get_env("DBT_CLOUD_ENVIRONMENT_ID"),
     description="Numeric ID of a dbt Cloud environment (default: DBT_CLOUD_ENVIRONMENT_ID environment variable)",
 )
 JOB_ID_FIELD = Field(
-    default_factory=lambda: os.environ["DBT_CLOUD_JOB_ID"],
+    default_factory=lambda: get_env("DBT_CLOUD_JOB_ID"),
     description="Numeric ID of a dbt Cloud job (default: DBT_CLOUD_JOB_ID environment variable)",
 )
 RUN_ID_FIELD = Field(
