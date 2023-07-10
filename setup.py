bcab362c6ad48701e60761bce480d477a34cba0c
Simo Tumelius <simo@datamie.fi>
2023-07-10 13:52:25 +0300

    Bump version to 0.7.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="dbt-cloud-cli",
-    version="0.7.5",
+    version="0.7.6",
     description="dbt Cloud command line interface (CLI)",
     long_description=README,
     long_description_content_type="text/markdown",
