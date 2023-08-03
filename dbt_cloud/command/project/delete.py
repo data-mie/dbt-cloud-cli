2cc36810ded206f56b78ecf84ce0b52c641a7c06
Simo Tumelius <simo@datamie.fi>
2023-08-03 12:52:16 +0300

    Change DbtCloudProjectDeleteCommand now uses API v3

--- dbt_cloud/command/project/delete.py
+++ dbt_cloud/command/project/delete.py
@@ -1,11 +1,12 @@
 import requests
-from pydantic import Field
+from pydantic import Field, PrivateAttr
 from dbt_cloud.command.command import DbtCloudAccountCommand
 
 
 class DbtCloudProjectDeleteCommand(DbtCloudAccountCommand):
     """Deletes a dbt Cloud project in a given account."""
 
+    _api_version: str = PrivateAttr("v3")
     project_id: int = Field(
         description="ID of the dbt Cloud project to delete.",
     )
